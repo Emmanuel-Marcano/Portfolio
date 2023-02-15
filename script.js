377150ee0d699eb74d2ceea9eff7d0a44cee3e5d
Emmanuel J. Marcano Perez <emmanueljulian.marcano@gmail.com>
2023-02-15 15:53:34 +0100

    added simple animations

--- script.js
+++ script.js
@@ -18,112 +18,25 @@ menuButton.addEventListener("click", () => {
 
 
 
+let observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
 
-
-
-let images = document.querySelectorAll(".svg")
-
-let back = document.querySelector(".back")
-let mid = document.querySelector(".mid")
-let up = document.querySelector(".top")
-let tippy = document.querySelector(".tippy")
-
-
- let observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-
-        if(entry.isIntersecting){
-            entry.target.classList.add("transition-card")
-            mid.classList.add("transition-mid")
-             up.classList.add("transition-top")
-            tippy.classList.add("transition-tippy")
-        }
-        else{
-            entry.target.classList.remove("transition-card")
-            mid.classList.remove("transition-mid")
-             up.classList.remove("transition-top")
-            tippy.classList.remove("transition-tippy")
-
-
-        }
-    } )
-
-
- })
-
- let layers =document.querySelector(".layers")
-
-//  observer.observe(layers)
-
- images.forEach((el) => {
-    observer.observe(el)
- })
-
-
-
-
-/*
-const techContainer = document.querySelectorAll(".tech-container")
-const techText = document.querySelectorAll(".tech-name")
-
-
-techContainer.forEach((container)=>{
-  container.addEventListener("mouseover", ()=>{
-
-    techText.forEach((el) => {
-
-      el.classList.add("show-tech-name")
-  
-  
-    })
+    if(entry.isIntersecting){
+      entry.target.classList.add('show')
+    } else{
+      entry.target.classList.remove('show')
+    }
 
   })
 })
 
-techContainer.forEach((container)=>{
-  container.addEventListener("mouseout", ()=>{
 
-    techText.forEach((el) => {
-
-      el.classList.remove("show-tech-name")
-  
-  
-    })
-
-  })
+let hiddenElements = document.querySelectorAll('.hidden')
+hiddenElements.forEach((el) => {
+  observer.observe(el)
 })
 
-/*
-
-techContainer.addEventListener("mouseover", (e) => {
-
-  techText.forEach((el) => {
-
-    el.classList.add("show-tech-name")
 
 
-  })
- 
- //techText.classList.add("show-tech-name")
 
 
-})
-
-techContainer.addEventListener("mouseout", (e) => {
-
-
- 
-  techText.forEach((el) => {
-
-    el.classList.remove("show-tech-name")
-
-
-  })
-
-
-})
-  */
-
-  
-  // When the user clicks on the button, scroll to the top of the document
